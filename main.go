2ed693a2deb5d6ba2bf929c7903ee7460aace2ff
pbudzyns <pawel.budzynski@nokia.com>
2018-10-28 14:46:28 +0100

    chat project frst commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "Port dla aplikacji.")
+	flag.Parse()
+	r := newRoom()
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	go r.run()
+	log.Println("Uruchamianie serwera WWW na porcie", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
