bc312e7f6561ce9eb416b64b777d0e05b0b16ce1
pbudzyns <pawel.budzynski@nokia.com>
2018-11-06 18:49:55 +0100

    messages are sending as json files

--- room.go
+++ room.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 	"trace"
 
+	"github.com/stretchr/objx"
+
 	"github.com/gorilla/websocket"
 )
 
 type room struct {
-	forward chan []byte
+	forward chan *message
 	join    chan *client
 	leave   chan *client
 	clients map[*client]bool
@@ -18,7 +20,7 @@ type room struct {
 
 func newRoom() *room {
 	return &room{
-		forward: make(chan []byte),
+		forward: make(chan *message),
 		join:    make(chan *client),
 		leave:   make(chan *client),
 		clients: make(map[*client]bool),
@@ -61,10 +63,17 @@ func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	authCookie, err := req.Cookie("auth")
+	if err != nil {
+		log.Fatal("Nie udało się pobrać cookie z danymi uwierzytelniającymi: ", err)
+		return
+	}
+
 	client := &client{
-		socket: socket,
-		send:   make(chan []byte, messageBufferSize),
-		room:   r,
+		socket:   socket,
+		send:     make(chan *message, messageBufferSize),
+		room:     r,
+		userData: objx.MustFromBase64(authCookie.Value),
 	}
 	r.join <- client
 	defer func() { r.leave <- client }()
